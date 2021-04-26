256c23a9380b3df821a48b4c7849923cbdc860ec
Aya Nemoto <md3b5zxntd@i.softbank.jp>
2021-04-26 20:01:15 +0900

    追加テーブルの作成_修正版#11

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: mysql
--- 生成日時: 2021 年 4 月 26 日 08:25
+-- 生成日時: 2021 年 4 月 26 日 10:55
 -- サーバのバージョン： 5.7.33
 -- PHP のバージョン: 7.4.16
 
@@ -20,23 +20,6 @@ SET time_zone = "+00:00";
 --
 -- データベース: `sample`
 --
-CREATE DATABASE IF NOT EXISTS `sample` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `sample`;
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `carts`
---
-
-CREATE TABLE `carts` (
-  `cart_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -64,67 +47,9 @@ CREATE TABLE `histories` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- テーブルの構造 `items`
---
-
-CREATE TABLE `items` (
-  `item_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `status` int(11) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- テーブルのデータのダンプ `items`
---
-
-INSERT INTO `items` (`item_id`, `name`, `stock`, `price`, `image`, `status`, `created`, `updated`) VALUES
-(32, 'ねこ', 2, 30000, 'ny1owjn3yqs0cow8w4ws.jpg', 1, '2019-08-09 09:12:30', '2021-04-24 15:26:30'),
-(33, 'ハリネズミ', 30, 50000, '16scmunsexdwcosw88g0.jpg', 1, '2019-08-09 09:13:33', '2019-08-09 09:13:33'),
-(34, 'hacked', 4, 18000, '5o58yiqy6x8o8c004kss.jpg', 1, '2021-04-20 11:15:47', '2021-04-21 21:44:02');
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `type` int(11) NOT NULL DEFAULT '2',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- テーブルのデータのダンプ `users`
---
-
-INSERT INTO `users` (`user_id`, `name`, `password`, `type`, `created`, `updated`) VALUES
-(1, 'sampleuser', 'password', 2, '2019-08-07 01:17:12', '2019-08-07 01:17:12'),
-(4, 'admin', 'admin', 1, '2019-08-07 10:45:11', '2019-08-07 10:45:11'),
-(5, '123456', '123456', 2, '2021-04-18 15:40:29', '2021-04-18 15:40:29');
-
 --
 -- ダンプしたテーブルのインデックス
 --
-
---
--- テーブルのインデックス `carts`
---
-ALTER TABLE `carts`
-  ADD PRIMARY KEY (`cart_id`),
-  ADD KEY `item_id` (`item_id`),
-  ADD KEY `user_id` (`user_id`);
 
 --
 -- テーブルのインデックス `details`
@@ -141,26 +66,8 @@ ALTER TABLE `histories`
   ADD KEY `user_id` (`user_id`);
 
 --
--- テーブルのインデックス `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`item_id`);
-
---
--- テーブルのインデックス `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
-
---
--- テーブルの AUTO_INCREMENT `carts`
---
-ALTER TABLE `carts`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- テーブルの AUTO_INCREMENT `histories`
@@ -169,27 +76,8 @@ ALTER TABLE `histories`
   MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- テーブルの AUTO_INCREMENT `items`
---
-ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-
---
--- テーブルの AUTO_INCREMENT `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- ダンプしたテーブルの制約
 --
-
---
--- テーブルの制約 `carts`
---
-ALTER TABLE `carts`
-  ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- テーブルの制約 `details`
